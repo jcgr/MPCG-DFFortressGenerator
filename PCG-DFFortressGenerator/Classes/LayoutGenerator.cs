9140c2be0238ef894281b301a7b95a371b4f7e00
Jacob G. <jcgr@itu.dk>
2014-12-04 00:49:11 +0100

    Generator now works with multiple levels

--- PCG-DFFortressGenerator/Classes/LayoutGenerator.cs
+++ PCG-DFFortressGenerator/Classes/LayoutGenerator.cs
@@ -26,14 +26,20 @@ namespace PCG_DFFortressGenerator.Classes
             NumberOfRooms = CalculateNumberOfRooms();
 
             GenerateEntrance(Map.MapLayers[Map.Z - 1]);
-            GenerateRooms(Map.MapLayers[Map.Z - 1]);
-//            for (int z = Map.Z - 1; z > 0; z--)
-//            {
-//                GenerateRooms(Map.MapLayers[z]);
-//            }
+//            GenerateRooms(Map.MapLayers[Map.Z - 1]);
+            for (int z = Map.Z - 1; z > 0; z--)
+            {
+                GenerateRooms(Map, z);
+                if (NumberOfRooms <= 0)
+                    break;
+            }
 
         }
 
+        /// <summary>
+        /// Calculates the number of rooms required.
+        /// </summary>
+        /// <returns>The number of rooms required.</returns>
         private int CalculateNumberOfRooms()
         {
             var rooms = 0;
@@ -61,51 +67,20 @@ namespace PCG_DFFortressGenerator.Classes
             if (tile2 == -1)
             {
                 if (Random.Next(10) % 2 == 0)
-                {
                     tile2 = tile1 + 1;
-                }
                 else
-                {
                     tile2 = tile1 - 1;
-                }
             }
 
             var entrance = layer.GenerateAndAddArea(tile2, 0, tile1, 0, new Entrance());
             layer.Entrance = entrance;
-//            var entrance = new Entrance();
-//            entrance.AddTile(layer.MapTiles[tile1, 0]);
-//            entrance.AddTile(layer.MapTiles[tile2, 0]);
-//            layer.SetTile(tile1, 0, Tile.TileType.Room, entrance);
-//            layer.SetTile(tile2, 0, Tile.TileType.Room, entrance);
-//            layer.AddArea(entrance);
-
-//            // TODO: This is test!
-//            Console.WriteLine("---------------");
-//            foreach (var areaTile in entrance.AreaTiles)
-//            {
-//                Console.WriteLine(areaTile);
-//            }
-//            Console.WriteLine("---------------");
-
-//            foreach (var areaTile in entrance.AreaTiles)
-//            {
-//                Console.WriteLine(areaTile);
-//            }
-//            Console.WriteLine("---------------");
-//            entrance.test();
-//            Console.WriteLine("---------------");
-
-//            var barracks = layer.GenerateAndAddArea(4, 4, 3, 3, new Barracks());
-
-//            var newArea = barracks.Copy();
-//            Console.WriteLine(newArea.AreaTiles[0].TileStatus + " - " + newArea.AreaTiles.Count);
-//            Console.WriteLine(entrance);
         }
 
-        private void GenerateRooms(TileLayer tileLayer)
+        private void GenerateRooms(Map map, int layer)
         {
             var done = false;
             var openPositions = new List<Position>();
+            var tileLayer = map.MapLayers[layer];
 
             foreach (var mapTile in tileLayer.MapTiles)
                 if (mapTile.TileStatus == Tile.TileType.NotDug)
@@ -113,12 +88,17 @@ namespace PCG_DFFortressGenerator.Classes
 
             while (!done)
             {
-                GenerateRoomAndPath(tileLayer, openPositions);
+                GenerateRoomAndPath(tileLayer, openPositions, layer);
                 done = IsLayerFinished(tileLayer, openPositions);
             }
+
+            if (NumberOfRooms <= 0)
+                return;
+
+            GenerateStairsToNextLevel(map, layer, openPositions);
         }
 
-        private void GenerateRoomAndPath(TileLayer tileLayer, List<Position> openPositions)
+        private void GenerateRoomAndPath(TileLayer tileLayer, List<Position> openPositions, int layer)
         {
             var generated = false;
             var tempArea = new Area();
@@ -147,87 +127,78 @@ namespace PCG_DFFortressGenerator.Classes
                     var checkX = startPosition.X + (combination.Item1 * 5);
                     var checkY = startPosition.Y + (combination.Item2 * 5);
 
-                    if (tileLayer.WithinLayer(checkX, checkY))
+                    if (!tileLayer.WithinLayer(checkX, checkY)) continue;
+                    if (!this.TilesOpenBetween(startPosition.X, startPosition.Y, checkX, checkY, tileLayer)) continue;
+
+                    // Create area
+                    tempArea = tileLayer.GenerateAndAddArea(
+                        startPosition.X + combination.Item1
+                        , startPosition.Y + combination.Item2
+                        , checkX - combination.Item1
+                        , checkY - combination.Item2
+                        , new Area());
+
+                    var wallTiles = new List<Tile>();
+
+                    // Build walls around room
+                    for (int xx = 0; Math.Abs(xx) <= Math.Abs(combination.Item1 * 5); xx++)
                     {
-                        if (this.TilesOpenBetween(startPosition.X, startPosition.Y, checkX, checkY, tileLayer))
+                        tileLayer.SetTile(startPosition.X + (combination.Item1 * xx), startPosition.Y, Tile.TileType.RoomWall, null);
+                        tileLayer.SetTile(startPosition.X + (combination.Item1 * xx), startPosition.Y + (combination.Item2 * 5), Tile.TileType.RoomWall, null);
+                                
+                        if (xx == 0 || Math.Abs(xx) == Math.Abs(combination.Item1 * 5)) continue;
+                        wallTiles.Add(tileLayer.MapTiles[startPosition.X + (combination.Item1 * xx), startPosition.Y]);
+                        wallTiles.Add(tileLayer.MapTiles[startPosition.X + (combination.Item1 * xx), startPosition.Y + (combination.Item2 * 5)]);
+                    }
+
+                    for (int yy = 0; Math.Abs(yy) <= Math.Abs(combination.Item2 * 5); yy++)
+                    {
+                        tileLayer.SetTile(startPosition.X, startPosition.Y + (combination.Item2 * yy), Tile.TileType.RoomWall, null);
+                        tileLayer.SetTile(startPosition.X + (combination.Item1 * 5), startPosition.Y + (combination.Item2 * yy), Tile.TileType.RoomWall, null);
+
+                        if (yy == 0 || Math.Abs(yy) == Math.Abs(combination.Item1 * 5)) continue;
+                        wallTiles.Add(tileLayer.MapTiles[startPosition.X, startPosition.Y + (combination.Item2 * yy)]);
+                        wallTiles.Add(tileLayer.MapTiles[startPosition.X + (combination.Item1 * 5), startPosition.Y + (combination.Item2 * yy)]);
+                    }
+
+                    // Attempt to generate path
+                    var pathGenerated = false;
+                    while (!pathGenerated)
+                    {
+                        // If a path cannot be generated, set it back to the original layout.
+                        if (wallTiles.Count <= 0)
                         {
-                            // Create area
-                            tempArea = tileLayer.GenerateAndAddArea(
-                                startPosition.X + combination.Item1
-                                , startPosition.Y + combination.Item2
-                                , checkX - combination.Item1
-                                , checkY - combination.Item2
-                                , new Area());
+                            tileLayer = originalLayout;
+                            return;
+                        }
 
-                            var wallTiles = new List<Tile>();
+                        var randomWallTileIndex = Random.Next(wallTiles.Count);
+                        var chosenWallTile = wallTiles[randomWallTileIndex];
+                        wallTiles.RemoveAt(randomWallTileIndex);
 
-                            // Build walls around room
-                            for (int xx = 0; Math.Abs(xx) <= Math.Abs(combination.Item1 * 5); xx++)
-                            {
-                                tileLayer.SetTile(startPosition.X + (combination.Item1 * xx), startPosition.Y, Tile.TileType.RoomWall, null);
-                                tileLayer.SetTile(startPosition.X + (combination.Item1 * xx), startPosition.Y + (combination.Item2 * 5), Tile.TileType.RoomWall, null);
+                        Console.WriteLine(layer);
+                        var path = Pathfinding.DijkstraFindPathTo(Map, chosenWallTile,
+                            tileLayer.Entrance.AreaTiles[0]);
                                 
-                                if (xx == 0 || Math.Abs(xx) == Math.Abs(combination.Item1 * 5)) continue;
-                                wallTiles.Add(tileLayer.MapTiles[startPosition.X + (combination.Item1 * xx), startPosition.Y]);
-                                wallTiles.Add(tileLayer.MapTiles[startPosition.X + (combination.Item1 * xx), startPosition.Y + (combination.Item2 * 5)]);
-                            }
-
-                            for (int yy = 0; Math.Abs(yy) <= Math.Abs(combination.Item2 * 5); yy++)
-                            {
-                                tileLayer.SetTile(startPosition.X, startPosition.Y + (combination.Item2 * yy), Tile.TileType.RoomWall, null);
-                                tileLayer.SetTile(startPosition.X + (combination.Item1 * 5), startPosition.Y + (combination.Item2 * yy), Tile.TileType.RoomWall, null);
-
-                                if (yy == 0 || Math.Abs(yy) == Math.Abs(combination.Item1 * 5)) continue;
-                                wallTiles.Add(tileLayer.MapTiles[startPosition.X, startPosition.Y + (combination.Item2 * yy)]);
-                                wallTiles.Add(tileLayer.MapTiles[startPosition.X + (combination.Item1 * 5), startPosition.Y + (combination.Item2 * yy)]);
-                            }
-
-                            // Attempt to generate path
-                            var pathGenerated = false;
-                            while (!pathGenerated)
-                            {
-                                // If a path cannot be generated, set it back to the original layout.
-                                if (wallTiles.Count <= 0)
-                                {
-                                    tileLayer = originalLayout;
-                                    return;
-                                }
-
-//                                Console.WriteLine(wallTiles.Count);
-
-                                // Get random wall tile to attempt.
-                                var randomWallTileIndex = Random.Next(wallTiles.Count);
-                                var chosenWallTile = wallTiles[randomWallTileIndex];
-                                wallTiles.RemoveAt(randomWallTileIndex);
-
-                                var path = Pathfinding.DijkstraFindPathTo(Map, chosenWallTile,
-                                    tileLayer.Entrance.AreaTiles[0]);
+                        if (path == null) continue;
                                 
-                                if (path == null) continue;
-                                
-                                foreach (var pathTile in path)
-                                {
-
-                                    if (pathTile.TileStatus == Tile.TileType.Room) continue;
-                                    tileLayer.SetTile(pathTile.Position.X, pathTile.Position.Y, Tile.TileType.Dug,
-                                        null);
-                                    pathGenerated = true;
-//                                Console.WriteLine(pathTile.Position);
-                                }
-                            }
-
-                            // remove positions from openPositions, as they are already in use.
-                            foreach (var areaTile in tempArea.AreaTiles)
-                                openPositions.Remove(areaTile.Position);
-
-                            NumberOfRooms--;
-                            generated = true;
-
-//                            Console.WriteLine(NumberOfRooms);
-
-                            break;
+                        foreach (var pathTile in path)
+                        {
+                            if (pathTile.TileStatus == Tile.TileType.Stairs) continue;
+                            if (pathTile.TileStatus == Tile.TileType.Room) continue;
+                            tileLayer.SetTile(pathTile.Position.X, pathTile.Position.Y, Tile.TileType.Dug,
+                                null);
+                            pathGenerated = true;
                         }
                     }
+
+                    foreach (var areaTile in tempArea.AreaTiles)
+                        openPositions.Remove(areaTile.Position);
+
+                    NumberOfRooms--;
+                    generated = true;
+
+                    break;
                 }
 
                 if (openPositions.Contains(startPosition))
@@ -235,6 +206,96 @@ namespace PCG_DFFortressGenerator.Classes
 
                 if (possibleCombinations.Count <= 0)
                     generated = true;
+            }
+        }
+
+        private void GenerateStairsToNextLevel(Map map, int layer, List<Position> openPositions)
+        {
+            var tileLayer = map.MapLayers[layer];
+            var originalLayout = tileLayer.Copy();
+            var generated = false;
+
+            while (!generated)
+            {
+                // Grab random start position
+                var startPosition = openPositions[Random.Next(openPositions.Count)];
+                var possibleCombinations = new List<Tuple<int, int>>
+                {
+                    new Tuple<int, int>(-1, 0),
+                    new Tuple<int, int>(0, -1),
+                    new Tuple<int, int>(0, 1),
+                    new Tuple<int, int>(1, 0)
+                };
+
+                for (int i = 0; i < 4; i++)
+                {
+                    // Choose random combination (up/down and left/right)
+                    var combinationIndex = Random.Next(possibleCombinations.Count);
+                    var combination = possibleCombinations[combinationIndex];
+                    possibleCombinations.RemoveAt(combinationIndex);
+
+                    // Create positions to check for being open.
+                    var checkX = startPosition.X + (combination.Item1);
+                    var checkY = startPosition.Y + (combination.Item2);
+
+                    if (!tileLayer.WithinLayer(checkX, checkY)) continue;
+                    if (!this.TilesOpenBetween(startPosition.X, startPosition.Y, checkX, checkY, tileLayer)) continue;
+
+                    var possibleTiles = new List<Tile>
+                    {
+                        tileLayer.MapTiles[startPosition.X, startPosition.Y],
+                        tileLayer.MapTiles[checkX, checkY]
+                    };
+
+                    // Attempt to generate path
+                    var pathGenerated = false;
+                    while (!pathGenerated)
+                    {
+                        // If a path cannot be generated, set it back to the original layout.
+                        if (possibleTiles.Count <= 0)
+                        {
+                            tileLayer = originalLayout;
+                            return;
+                        }
+
+                        var randomWallTileIndex = Random.Next(possibleTiles.Count);
+                        var chosenWallTile = possibleTiles[randomWallTileIndex];
+                        possibleTiles.RemoveAt(randomWallTileIndex);
+
+                        var path = Pathfinding.DijkstraFindPathTo(Map, chosenWallTile,
+                            tileLayer.Entrance.AreaTiles[0]);
+
+                        if (path == null) continue;
+
+                        foreach (var pathTile in path)
+                        {
+                            if (pathTile.TileStatus == Tile.TileType.Stairs) continue;
+                            if (pathTile.TileStatus == Tile.TileType.Room) continue;
+                            tileLayer.SetTile(pathTile.Position.X, pathTile.Position.Y, Tile.TileType.Dug,
+                                null);
+                            pathGenerated = true;
+                        }
+                    }
+
+                    // Change tiles to be stairs
+                    tileLayer.MapTiles[startPosition.X, startPosition.Y].TileStatus = Tile.TileType.Stairs;
+                    tileLayer.MapTiles[checkX, checkY].TileStatus = Tile.TileType.Stairs;
+
+                    // Do the same for the next layer
+                    if (map.MapLayers[layer - 1] != null)
+                    {
+                        var newEntrance = new Area();
+                        newEntrance.AddTile(map.MapLayers[layer - 1].MapTiles[startPosition.X, startPosition.Y]);
+                        newEntrance.AddTile(map.MapLayers[layer - 1].MapTiles[checkX, checkY]);
+                        map.MapLayers[layer - 1].MapTiles[startPosition.X, startPosition.Y].TileStatus = Tile.TileType.Stairs;
+                        map.MapLayers[layer - 1].MapTiles[checkX, checkY].TileStatus = Tile.TileType.Stairs;
+                        map.MapLayers[layer - 1].Entrance = newEntrance;
+                    }
+
+                    generated = true;
+
+                    break;
+                }
             }
         }
 
@@ -267,7 +328,7 @@ namespace PCG_DFFortressGenerator.Classes
         {
             if (NumberOfRooms <= 0)
                 return true;
-            if (openPositions.Count <= tileLayer.X + tileLayer.Y)
+            if (openPositions.Count <= (tileLayer.X * tileLayer.Y) * 0.1)
                 return true;
 
             return false;
